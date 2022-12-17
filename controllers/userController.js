a07c4dfdc9e60476007d649d822fd2eb99def255
Miles Thomas <milesman2013@gmail.com>
2022-12-17 15:36:25 +1100

    user delete and update routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User.js');
+const { User, Thought } = require('../models');
 const { ObjectId } = require('mongoose').Types;
 
 module.exports = {
@@ -19,4 +19,20 @@ module.exports = {
         .then((userData) => res.json(userData))
         .catch((err) => res.status(500).json(err));
     },
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: ObjectId(req.params.userId) })
+        .then((user) => !user ? res.status(404).json({ message: 'Sorry, this user could not be found!' }) : Thought.deleteMany({ _id: { $in: user.thoughts } }))
+        .then(() => res.json({ message: 'User has been successfully deleted' }))
+        .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: ObjectId(req.params.userId) },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => !user ? res.status(404).json({ message: 'Sorry, this user could not be found!' }) : res.json(user))
+        .catch((err) => res.status(500).json(err));
+    }
+    
 };
