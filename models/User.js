16d41771b3fe1a3189c9fca5a4e62e298e56682b
Miles Thomas <milesman2013@gmail.com>
2022-12-17 14:06:36 +1100

    userschema

--- models/User.js
+++ models/User.js
@@ -3,10 +3,20 @@ const { thoughtSchema } = require('./Thought');
 
 const userSchema = new Schema({
     username: {
-
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
     email: {
-
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
+            }
+        }
     },
     thoughts: [thoughtSchema],
     friends: [userSchema]
