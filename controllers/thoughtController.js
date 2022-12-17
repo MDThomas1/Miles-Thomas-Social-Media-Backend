97fc1ee9d6a5416d86755ba3b82e889e6b484d97
Miles Thomas <milesman2013@gmail.com>
2022-12-17 15:41:04 +1100

    update search paarameters

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require('../models/Thought.js')
+const { Thought, User } = require('../models')
 const { ObjectId } = require('mongoose').Types;
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
         .catch((err) => res.status(500).json(err));
     },
     getOneThought(req, res) {
-        Thought.findOne({ _id: ObjectId(req.params.userId) })
+        Thought.findOne({ _id: ObjectId(req.params.id) })
         .select('-__v')
         .then((thought) =>
         !thought ? res.status(404).json({ message: 'Sorry, this thought could not be found' }) : res.json(thought))
@@ -19,4 +19,27 @@ module.exports = {
         .then((thoughtData) => res.json(thoughtData))
         .catch((err) => res.status(500).json(err));
     },
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.studentId })
+        .then((student) =>
+            !student
+              ? res.status(404).json({ message: 'No such student exists' })
+              : User.findOneAndUpdate(
+                  { thoughts: req.params.studentId },
+                  { $pull: { thought: req.params.studentId } },
+                  { new: true }
+                )
+          )
+          .then((course) =>
+            !course
+              ? res.status(404).json({
+                  message: 'Student deleted, but no courses found',
+                })
+              : res.json({ message: 'Student successfully deleted' })
+          )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }    
 };
