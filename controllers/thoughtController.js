ac078a5171cfc271695545c5e5b65f5c82812519
Miles Thomas <milesman2013@gmail.com>
2022-12-19 13:03:07 +1100

    reaction routes added to thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -16,17 +16,28 @@ module.exports = {
     },
     createNewThought(req, res) {
         Thought.create(req.body)
-        .then((thoughtData) => res.json(thoughtData))
-        .catch((err) => res.status(500).json(err));
+            .then((thought) => {
+            return User.findOneAndUpdate(
+              { username: req.body.username },
+              { $addToSet: { thoughts: thought } },
+              { new: true }
+            );
+        })
+        .then((user) => !user ? res.status(404).json({ message: 'No user could be found with the provided username' }) : res.json('New thought has been successfully uploaded')
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
     deleteThought(req, res) {
         Thought.findOneAndRemove({ _id: ObjectId(req.params.id) })
-        .then((thought) =>
-            !thought
-              ? res.status(404).json({ message: 'Sorry, this thought could not be found' })
+        .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'This thought could not be found within a user' })
               : User.findOneAndUpdate(
-                  { thoughts: req.params.studentId },
-                  { $pull: { thought: req.params.studentId } },
+                  { thoughts: ObjectId(req.params.id) },
+                  { $pull: { thoughts: ObjectId(req.params.id) } },
                   { new: true }
                 )
           )
@@ -50,5 +61,31 @@ module.exports = {
         )
         .then((thought) => !thought ? res.status(404).json({ message: 'Sorry, this thought could not be found!' }) : res.json(thought))
         .catch((err) => res.status(500).json(err));
-    }    
+    },
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: ObjectId(req.params.id) },
+            { $addToSet: { reactions: req.body } },
+            { new: true }
+        )
+        .then((thought) => !thought ? res.status(404).json({ message: 'Sorry, this thought could not be found!' }) : res.json('New reaction has been successfully uploaded')
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    deleteReaction(req, res) {
+        Thought.findOneAndRemove(
+            { reactions: ObjectId(req.params.id) },
+            { $pull: { thoughts: ObjectId(req.params.id) } },
+            { new: true }
+        )
+        .then((user) => !user ? res.status(404).json({ message: 'No user could be found with the provided username' }) : res.json('New thought has been successfully uploaded')
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 };
